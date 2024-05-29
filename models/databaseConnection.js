56eaa845565d0c80f936c852f95f207b02c5c30b
pratham meena <yashmeena274@gmail.com>
2024-05-29 16:13:18 +0530

    done some error handling in db connection

--- models/databaseConnection.js
+++ models/databaseConnection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
-const { catchAsyncError } = require('../middlewares/catchAsyncError')
 
-exports.databaseConnection = catchAsyncError(async()=>{
-    await mongoose.connect(process.env.MONGODBURL)
-    console.log('Database Connection Established!')
-})
+exports.databaseConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODBURL)
+        console.log('Database Connection Established!')
+    } catch (error) {
+        console.log(err.message)
+    }
+}
